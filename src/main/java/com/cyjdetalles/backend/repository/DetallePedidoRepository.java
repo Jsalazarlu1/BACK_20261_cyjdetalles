aef778250bd92d2cd383341b40ae1d8cd619a0d7
Jsalazarlu1 <jorgesalazar4688@gmail.com>
2026-05-14 20:14:43 -0500

    Delete src/main/java/com/cyjdetalles/backend directory

--- src/main/java/com/cyjdetalles/backend/repository/DetallePedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cyjdetalles.backend.repository;
-
-import com.cyjdetalles.backend.model.DetallePedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Long> {
-}
